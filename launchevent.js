ff2fb43af0a678267c7f4660de32c651a3104192
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-05 15:38:23 +0530

    Update launchevent.js

--- launchevent.js
+++ launchevent.js
@@ -99,7 +99,7 @@ function onMessageSendHandler(event) {
 }
 
 function sendFormData(formData, event) {
-  fetch("https://sent-mail-download.onrender.com/receive_email", {
+  fetch("http://127.0.0.1:5000/receive_email", {
     method: "POST",
     body: formData
   })
