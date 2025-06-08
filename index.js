5a7a6233571efe0acb11d5de35e043ff0c8c9230
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-08 20:05:29 +0530

    CORS headers added

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Manual CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 // Serve all static files from the current directory
 app.use(express.static(path.join(__dirname)));
 
